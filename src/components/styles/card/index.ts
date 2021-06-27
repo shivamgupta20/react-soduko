a20604c70b50184df3ccb2c6e22e0298945dd3d2
shivamgupta20 <shivamgupta20@gmail.com>
2021-06-27 14:54:41 +0530

    added card

---
+++ src/components/styles/card/index.ts
@@ -0,0 +1,12 @@
+import styled, { css } from 'styled-components'
+
+export const Card = styled.div`
+   ${({ theme }) => css`
+        background-color: ${theme.colors.white};
+        border-radius: 15px;
+        display: flex;
+        flex: 1;
+        max-height: fit-content;
+        padding: 15px;
+   `}
+`
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom'
 import { unregister } from './core/service-worker'
 import { GlobalStyles, theme } from './styles'
 import { ThemeProvider } from 'styled-components'
+import { Content, Title, Card } from './components'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>Hello World!</div>
+    <Content>
+      <Title>Sudoku</Title>
+      <Card> in progress</Card>
+    </Content>
   </ThemeProvider>,
   document.getElementById('root')
 )
